d9339a9d4b425b628bc06f364b443c5a4d28ce0d
Khanhpear97 <khanhpear97@gmail.com>
2023-10-19 14:38:13 +0700

    first commit

---
+++ apj_2023_junit_calculator/src/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package apj_2023_junit_calculator.src;
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int sub(int first, int second){
+        return first - second;
+    }
+}
